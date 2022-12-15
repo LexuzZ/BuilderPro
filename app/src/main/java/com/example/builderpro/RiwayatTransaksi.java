f5bb41cb96af20518681605a5bd0e0a4c1f0c977
raflyelfahmi <m.rafly9g22.sooko1@gmail.com>
2022-12-15 09:36:11 +0700

    Ubah RiwayatTransaksi.java

--- app/src/main/java/com/example/builderpro/RiwayatTransaksi.java
+++ app/src/main/java/com/example/builderpro/RiwayatTransaksi.java
@@ -3,6 +3,7 @@ package com.example.builderpro;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 
 public class RiwayatTransaksi extends AppCompatActivity {
 
@@ -10,5 +11,6 @@ public class RiwayatTransaksi extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_riwayat_transaksi);
+        Log.d("TES RAFLY", "TES RAFLY");
     }
 }
